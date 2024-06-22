1ba351dd77f5a94f82dd7bc56039753d403dd18c
TruePelmen <oleksii.senyshyn@lnu.edu.ua>
2024-06-22 14:13:39 +0300

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,16 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.6")
+
+    implementation("io.coil-kt:coil-compose:1.3.2")
+
 }
